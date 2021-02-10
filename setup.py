181ce8d242f71aa16e8cb088a98ea5cf93483468
Marcin Morys <marcin.morys.1@us.af.mil>
2021-02-10 12:35:23 -0500

    Added packagesetup files

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+DESCRIPTION = 'A Python application that runs Coral object detection behind an HTTP server. This is meant to allow interaction with Coral image processing in the same manner as with Deepstack, using POST requests.'
+
+# Setting up
+setup(
+    # the name must match the folder name 'verysimplemodule'
+    name="coral-detection-service",
+    version=VERSION,
+    author="Marcin Morys",
+    author_email="<marcin.m.morys@gmail.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['requests',
+                      'numpy',
+                      'pillow',
+                      'pycoral'],
+    dependency_links=['https://google-coral.github.io/py-repo/'],
+    keywords=['python', 'coral']
+)
